0b156b530444ec60a399633030fd83f326164c8f
zhaoyehua <zhaoyh6@asiainfo.com>
2021-06-04 10:03:07 +0800

    feat:add test case for project
    Issue-ID: USECASEUI-525
    Change-Id: I9f7e792f251b1c13c341f2ece31c7dc13e2703ce
    Signed-off-by: zhaoyehua <zhaoyh6@asiainfo.com>

--- nlp/Test_1.py
+++ nlp/Test_1.py
@@ -12,11 +12,14 @@ sys.modules['optimization'] = mock.Mock()
 sys.modules['tokenization'] = mock.Mock()
 sys.modules['tensorflow'] = mock.Mock()
 import api_squad_offline
+import api_squad_online
 import tokenization
 import tensorflow
 import api_squad
 import requests
 import create_squad_features
+import modeling
+import json
 
 
 class TestApiSquad(unittest.TestCase):
@@ -63,11 +66,80 @@ class TestApiSquad(unittest.TestCase):
         tokenizer.tokenize = mock.Mock(return_value="T")
         tokenizer.convert_tokens_to_ids = mock.Mock(return_value=['X', 'X', ' ', 'B', 'a', 'n', 'k', ' ', 'o', 'f', 'f', 'i', 'c', 'i', 'a', 'l', ' ', 'n', 'e', 't', 'w', 'o', 'r', 'k', ' ', 'a', 't'])
         tokenization.printable_text = mock.Mock(return_value="XX")
-        result = api_squad.convert_examples_to_features(examples, tokenizer, max_seq_length,
-                                 doc_stride, max_query_length, is_training,
-                                 output_fn)
+        result = api_squad.convert_examples_to_features(examples, tokenizer, max_seq_length, doc_stride, max_query_length, is_training, output_fn)
         self.assertEqual([], [])
         # examples = ['qas_id': '50Max Number of UEs', 'question_text': "Max Number of UEs", doc_tokens: [XX Bank official network at Financial Street, Xicheng District, Beijing; access number 10000; non-shared network; stationary network; the latency is as low as possible, upload and download bandwiths are 10Gb], start_position: 12, end_position: 12, is_impossible: 0]
+
+    def test_create_model(self):
+        model = mock.Mock()
+        modeling.BertModel = mock.Mock(return_value=model)
+        final_hidden=mock.Mock()
+        model.get_sequence_output = mock.Mock(return_value=final_hidden)
+        modeling.get_shape_list = mock.Mock(return_value=[1,2,3])
+        tensorflow.truncated_normal_initializer = mock.Mock(return_value=mock.Mock())
+        tensorflow.get_variable = mock.Mock(return_value=mock.Mock())
+        tensorflow.zeros_initializer = mock.Mock(return_value=mock.Mock())
+        tensorflow.reshape = mock.Mock(return_value=mock.Mock())
+        tensorflow.matmul = mock.Mock(return_value=mock.Mock())
+        tensorflow.nn.bias_add = mock.Mock(return_value=mock.Mock())
+        tensorflow.reshape = mock.Mock(return_value=mock.Mock())
+        tensorflow.transpose = mock.Mock(return_value=mock.Mock())
+        tensorflow.unstack = mock.Mock(return_value=[1,2])
+        result = api_squad.create_model(None, None, None, None, None, None)
+        self.assertEqual([], [])
+
+    # def test_model_fn_builder(self):
+    #     tensorflow.logging.info = mock.Mock(return_value=mock.Mock())
+    #     tensorflow.trainable_variables = mock.Mock(return_value=["a","b"])
+    #     modeling.get_assignment_map_from_checkpoint = mock.Mock(return_value=(","))
+    #     features = json.loads('{"input_ids":[[1,1,1,1,1,1]],"input_mask":[[1,1,1,1,1,1]],"segment_ids":[[1,1,1,1,1,1]]}')
+    #     for name in sorted(features.keys()):
+    #         features[name].shape= mock.Mock(return_value=1)
+    #
+    #     mode = 'train'
+    #     tensorflow.estimator.ModeKeys.TRAIN = mock.Mock(return_value='train')
+    #     api_squad.create_model = mock.Mock(return_value=([1,2,3,4,5,6,7,8,9,10],[[11,12,13,14,15,16,17,18,19,20]]))
+    #
+    #
+    #     api_squad.model_fn_builder(None,True,None,1,1,True,None)(features, None, mode, None)
+    #     self.assertEqual(1,1)
+
+    def test_input_fn_builder(self):
+        params = {"batch_size":2}
+        d = mock.Mock()
+        tensorflow.data.TFRecordDataset = mock.Mock(return_value=d)
+        d.repeat = mock.Mock(return_value=d)
+        d.shuffle = mock.Mock(return_value=d)
+        def tmp():
+            return 1
+
+        tensorflow.contrib.data.map_and_batch = mock.Mock(return_value=tmp)
+
+        example = {"batch_size":2}
+        tensorflow.parse_single_example = mock.Mock(return_value=example)
+
+
+        result = api_squad.input_fn_builder(None, 2, True, None)(params)
+
+        self.assertEqual(1, 1)
+
+
+
+    def test_get_best_indexes(self):
+        logits = [0,1,2,3,4,5,6,7,8,9,10]
+        n_best_size = 20
+        result = api_squad._get_best_indexes(logits, n_best_size)
+        self.assertEqual(result,[10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+
+    def test_compute_softmax(self):
+        result = api_squad._compute_softmax([1,2,3,4])
+        self.assertEqual(1, 1)
+
+    def test_validate_flags_or_throw(self):
+        tokenization.validate_case_matches_checkpoint = mock.Mock(return_value="")
+        api_squad.validate_flags_or_throw(None)
+        self.assertEqual(1,1)
+
 
 class TestApiSquadOffline(unittest.TestCase):
     def test_serving_input_fn(self):
