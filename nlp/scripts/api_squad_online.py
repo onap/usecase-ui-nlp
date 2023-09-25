0724c32c41325b76f6eed90e08d30fa2efb05664
Zhen <liz779@chinatelecom.cn>
2023-09-25 17:33:07 +0800

    update to python 3.11
    Issue-ID: USECASEUI-822
    Signed-off-by: Zhen <liz779@chinatelecom.cn>
    Change-Id: Iafc179181c5d64353e36f813558c79bd57562b85

--- nlp/scripts/api_squad_online.py
+++ nlp/scripts/api_squad_online.py
@@ -15,6 +15,7 @@ from flask import Flask, abort, request, jsonify
 import os
 from global_setting import tokenizer_ch, CUDA_VISIBLE_DEVICES
 from create_squad_features import get_squad_feature_result
+from transformers import pipeline
 
 
 app = Flask(__name__)
@@ -76,7 +77,46 @@ class AI2Flask:
     def start(self):
         self.app.run(host="0.0.0.0", port=self.port, threaded=True)
 
+class QuestionAnswerer:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_file(self):
+        res = []
+        read_res = csv.reader(open(self.file_path, "r", encoding="UTF-8-sig"))
+        for row in read_res:
+            res.append(row)
+        return res
+
+    def answerer(self, res):
+        question_answerer = pipeline("question-answering", model='distilbert-base-uncased-distilled-squad')
+        questions = [
+            "How much bandwidth is required?",
+            "Where are you from?",
+            "What is your destination?"
+        ]
+        for context in res:
+            result = question_answerer(question=questions[0], context=context[0])
+            result2 = question_answerer(question=questions[1], context=context[0])
+            result3 = question_answerer(question=questions[2], context=context[0])
+            print(
+                questions[0],
+                f"Answer: '{result['answer']}', score: {round(result['score'], 4)}, start: {result['start']}, end: {result['end']}")
+            print(
+                questions[1],
+                f"Answer: '{result2['answer']}', score: {round(result2['score'], 4)}, start: {result2['start']}, end: {result2['end']}")
+            print(
+                questions[2],
+                f"Answer: '{result3['answer']}', score: {round(result3['score'], 4)}, start: {result3['start']}, end: {result3['end']}")
+
+
+
 
 if __name__ == '__main__':
     port = sys.argv[1]
     AI2Flask(port=port).start()
+    file_path = '../TestFile.csv'
+    QA = QuestionAnswerer(file_path)
+    res = QA.read_file()
+
+    QA.answerer(res)
